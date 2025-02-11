6e88d5675b30bf848ac8135fb1a63481b8951dc2
Abdul Rehman <amcount000@gmail.com>
2025-02-11 20:15:34 +0530

    Update

--- src/Components/AuthButton.js
+++ src/Components/AuthButton.js
@@ -8,13 +8,13 @@ function AuthButtton() {
 
 	return(<div id='auth-container'>
 		{!user ? (
-			<form onClick={(e) => e.preventDefault()}>
+			<form>
 				<input type='text' placeholder='Enter Username' value={userName} onChange={(e) => setUserName(e.target.value)} required />
 				<input type='password' placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)} required />
 				<button id='login-btn' onClick={() => login(userName)}>Login</button>
 				</form>
 			) : (
-				<button type='submit' id='logout-btn' onClick={logout}>Logout</button>
+				<button id='logout-btn' onClick={logout}>Logout</button>
 			)}
 	</div>);
 }
